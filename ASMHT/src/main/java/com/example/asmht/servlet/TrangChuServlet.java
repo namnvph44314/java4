397c50c2e4bef07ba43b6fb6568ddc491488ad6b
Van Nam <nvnam11a@gmail.com>
2023-12-22 17:03:10 +0700

    'them'

--- ASMHT/src/main/java/com/example/asmht/servlet/TrangChuServlet.java
+++ ASMHT/src/main/java/com/example/asmht/servlet/TrangChuServlet.java
@@ -28,6 +28,7 @@ public class TrangChuServlet extends HttpServlet {
             this.thongTin(request,response);
         }else if(uri.contains("/trangChu/hienThi")){
             this.trangChu(request,response);
+            System.out.println("hell");
         }
     }
     private void trangChu(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
