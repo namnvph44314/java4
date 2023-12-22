db2b67e641044750805a0338b8b102ff8dd3bdea
Van Nam <nvnam11a@gmail.com>
2023-12-22 23:13:21 +0700

    'nam_code'

--- ASMHT/src/main/java/com/example/asmht/servlet/TrangChuServlet.java
+++ ASMHT/src/main/java/com/example/asmht/servlet/TrangChuServlet.java
@@ -28,6 +28,7 @@ public class TrangChuServlet extends HttpServlet {
             this.thongTin(request,response);
         }else if(uri.contains("/trangChu/hienThi")){
             this.trangChu(request,response);
+            System.out.println("nam_code");
         }
     }
     private void trangChu(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
